be5ffe81455c7e58f0bea51d4d979b37497d4921
tkbreeze <tadoufudao@gmail.com>
2023-02-16 23:30:51 +0900

    #24 fix bug

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,15 +1,15 @@
 class GamesController < ApplicationController
     def index
         @games = params[:name].present? ? Hardware.find(params[:name]).games.order(release_date: :desc).page(params[:page]).per(5) : Game.order(release_date: :desc).page(params[:page]).per(5)
-        @hardwares = Hardware.where(id: GameHardware.where(game_id: Game.where(id: @games)))
-        @genres = Genre.where(id: GameGenre.where(game_id: Game.where(id: @games)))
+        #@hardwares = Hardware.where(id: GameHardware.where(game_id: Game.where(id: @games)))
+        #@genres = Genre.where(id: GameGenre.where(game_id: Game.where(id: @games)))
     end
 
     def show
         @game = Game.find(params[:id])
-        @hardwares = Hardware.where(id: GameHardware.where(game_id: Game.where(id: @game)))
+        @hardwares = Hardware.joins(:game_hardwares).where(game_hardwares: {game_id: @game})
         #@hardware = Hardware.where(game_id: @game)
-        @genres = Genre.where(id: GameGenre.where(game_id: Game.where(id: @game)))
+        @genres = Genre.joins(:game_genres).where(game_genres: {game_id: @game})
         @reviews = Review.where(game_id: @game).includes(:user).order(created_at: :desc).page(params[:page]).per(5)
     end
 
