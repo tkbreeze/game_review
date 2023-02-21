421a633b760224f51d9bddfcb95e15849a099251
tkbreeze <tadoufudao@gmail.com>
2023-02-21 19:19:53 +0900

    #26 add review request

--- spec/factories/reviews.rb
+++ spec/factories/reviews.rb
@@ -4,10 +4,14 @@ FactoryBot.define do
     sequence(:body) {|n| "test#{n}"}
     score {8.4}
     classification_flag {"f"}
-    good_point {0}
-    bad_point {0}
+    good_point {'not_defined'}
+    bad_point {'not_defined'}
     sequence(:title) {|n| "test_title#{n}"}
     association :user
     association :game
+
+    trait :invalid do
+      play_hour {nil}
+    end
   end
 end
