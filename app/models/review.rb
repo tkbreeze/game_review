fb50027c474c054f28e7a90e5ab2ebb26a4c12f0
tkbreeze <tadoufudao@gmail.com>
2023-02-23 15:57:43 +0900

    #34 fix good_point, bad_point

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,8 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :game
   belongs_to :user
-  enum good_point: {not_defined: 0, game_property: 1, graphic: 2, world: 3, contents: 4, BGM: 5}, _prefix: true
-  enum bad_point: {not_defined: 0, game_property: 1, graphic: 2, world: 3, contents: 4, BGM: 5}, _prefix: true
+  enum good_point: {not_defined: 0, nothing: 1, game_property: 2, graphic: 3, world: 4, contents: 5, BGM: 6}, _prefix: true
+  enum bad_point: {not_defined: 0, nothing: 1, game_property: 2, graphic: 3, world: 4, contents: 5, BGM: 6}, _prefix: true
   #一意性制約
   validates :user_id, uniqueness: {scope: :game_id}
   validates :play_hour, presence: true, numericality: {greater_than_or_equal_to: 0.0}
