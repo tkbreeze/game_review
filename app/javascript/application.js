d10ac79bff6e4857f93bc7981d0a33aa28b4a2eb
tkbreeze <tadoufudao@gmail.com>
2023-02-25 01:00:35 +0900

    #34 add tooltip

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,7 +4,5 @@ import "./controllers"
 import * as bootstrap from "bootstrap"
 import "chartkick/chart.js"
 
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
-})
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
