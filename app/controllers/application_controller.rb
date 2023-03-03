a630563e3e8319017d6f74bbab592cf4113159cf
tkbreeze <tadoufudao@gmail.com>
2023-03-03 23:24:46 +0900

    #39 add user_params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
-  def after_sign_in_path_for(resource) 
-    super
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  protected
+  def configure_permitted_parameters
+    # サインアップ時にnameのストロングパラメータを追加
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    # アカウント編集の時にnameのストロングパラメータを追加
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 end
