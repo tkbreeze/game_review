d7e2462d97e1c8ea3763cd4d005a58ee0c7f392b
tkbreeze <tadoufudao@gmail.com>
2023-03-05 12:14:58 +0900

    #41 set avater column and uploader class

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_03_074424) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_05_030855) do
   create_table "game_genres", force: :cascade do |t|
     t.integer "game_id", null: false
     t.integer "genre_id", null: false
@@ -75,6 +75,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_03_074424) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name", null: false
+    t.string "avater"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
