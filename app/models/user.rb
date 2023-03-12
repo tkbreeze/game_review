44318e957ca1bf4583983243112826c3817d11fc
tkbreeze <tadoufudao@gmail.com>
2023-03-12 19:09:18 +0900

    #52 set up model association

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :reviews
+  has_many :likes
   validates :name, presence: true
   mount_uploader :avatar, AvatarUploader
 end
