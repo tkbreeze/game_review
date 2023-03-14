4ece7b4deceebf74c3b2a2e335344fc7420509ec
tkbreeze <tadoufudao@gmail.com>
2023-03-14 22:10:19 +0900

    #52 add js

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,12 +1,12 @@
 class LikesController < ApplicationController
-  before_action :post_params
+  before_action :review_params
   before_action :authenticate_user!
   def create
-    Like.create(user_id: current_user.id, post_id: params[:id])
+    Like.create(user_id: current_user.id, review_id: params[:id])
   end
 
   def destroy
-    Like.find_by(user_id: current_user.id, post_id: params[:id]).destroy
+    Like.find_by(user_id: current_user.id, review_id: params[:id]).destroy
   end
 
   private
